9652a55619198dc99e11b98fc08551badcec9c7f
Ziad Abdallah Mahmoud <ersmbusiness@gmail.com>
2026-07-01 00:29:51 +0300

    Avalonia Applied

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,37 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Ersm_Animation_App;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void PencilButton_Click(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Implement Pencil tool logic
+    }
+
+    private void BrushButton_Click(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Implement Brush tool logic
+    }
+
+    private void EraserButton_Click(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Implement Eraser tool logic
+    }
+
+    private void ClearButton_Click(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Implement Clear Canvas logic
+    }
+
+    private void PickerButton_Click(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Implement Color Picker logic
+    }
+}
